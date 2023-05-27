a1c27c82326a4e325ed63f784bd624ee83acebbe
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-05-27 19:18:40 -0300

    docs(c): add explain comment

--- read-listener.c
+++ read-listener.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+* This is my first example using while(1), it may be upgraded or refactored
+* along the time
+*/
+
 void read_listener () {
   char input = '\0';
   while(1) {
