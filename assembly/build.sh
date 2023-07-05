05e8b77d503dbcd8f15d0ca6bb14a4924db7ec9d
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-07-04 23:51:19 -0300

    chore(asm): implement hello world
    add line of execution of the binary in build.sh
    start adding the helloworld-related instructions

--- assembly/build.sh
+++ assembly/build.sh
@@ -1,2 +1,3 @@
 nasm -f elf64 hello.asm
 ld -s -o hello hello.o
+./hello
--- assembly/hello.asm
+++ assembly/hello.asm
@@ -2,12 +2,20 @@
 ; Link - ld -s -o hello hello.o
 
 section .data
-
+  msg db "Hello World!"
+  tam equ $- msg
 section .text
 
 global _start
 
 _start:
+  mov eax, 0x4
+  mov ebx, 0x1
+  mov ecx, msg
+  mov edx, tam
+  int 0x80
+
   mov eax, 0x1 ; Program ending
   mov ebx, 0x0 ; Program exited 0
   int 0x80
+
