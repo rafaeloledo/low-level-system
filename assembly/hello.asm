bb5fcd33d3603398e96720a56b73e34f7e16188f
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-07-04 23:56:00 -0300

    chore(asm): add comments for future sake

--- assembly/hello.asm
+++ assembly/hello.asm
@@ -4,18 +4,19 @@
 section .data
   msg db "Hello World!"
   tam equ $- msg
+
 section .text
 
 global _start
 
 _start:
-  mov eax, 0x4
-  mov ebx, 0x1
-  mov ecx, msg
-  mov edx, tam
-  int 0x80
+  mov eax, 0x4 ; tell to send
+  mov ebx, 0x1 ; to stdout
+  mov ecx, msg ; the msg
+  mov edx, tam ; specified length
+  int 0x80 ; execute
 
   mov eax, 0x1 ; Program ending
   mov ebx, 0x0 ; Program exited 0
-  int 0x80
+  int 0x80 ; execute
 
