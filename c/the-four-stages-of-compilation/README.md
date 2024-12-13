04f5689674d8f64e4933c70fad6b34e2c46d8eed
rafaeloledo <rafaeloliveiraledo@gmail.com>
2024-12-13 12:44:22 -0300

    feat: add clang example

---
+++ c/the-four-stages-of-compilation/README.md
@@ -0,0 +1,8 @@
+# The four stages of compilation
+
+```
+clang -E preprocess.c -o preprocess.i
+clang -S preprocess.i -o compile.s
+clang -c compile.s -o assemble.o
+ld assemble.o -o assemble
+```
---
+++ c/the-four-stages-of-compilation/compile.s
@@ -0,0 +1,37 @@
+	.text
+	.file	"preprocess.c"
+	.globl	main                            # -- Begin function main
+	.p2align	4, 0x90
+	.type	main,@function
+main:                                   # @main
+	.cfi_startproc
+# %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	movl	$0, -4(%rbp)
+	leaq	.L.str(%rip), %rdi
+	movb	$0, %al
+	callq	printf@PLT
+	xorl	%eax, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	.cfi_def_cfa %rsp, 8
+	retq
+.Lfunc_end0:
+	.size	main, .Lfunc_end0-main
+	.cfi_endproc
+                                        # -- End function
+	.type	.L.str,@object                  # @.str
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.L.str:
+	.asciz	"Hello, World!\n"
+	.size	.L.str, 15
+
+	.ident	"clang version 18.1.8"
+	.section	".note.GNU-stack","",@progbits
+	.addrsig
+	.addrsig_sym printf
